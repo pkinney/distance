5c0847c839aab546f3477f545d02da6f6e7b83eb
Powell Kinney <powell@vin.li>
2016-03-25 18:01:49 -0500

    fix mix.exs github link

--- mix.exs
+++ mix.exs
@@ -3,30 +3,35 @@ defmodule Distance.Mixfile do
 
   def project do
     [app: :distance,
-     version: "0.0.1",
+     version: "0.1.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp description do
+    """
+    Various distance calculations
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Powell Kinney"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/pkinney/distance"}
+    ]
   end
 end

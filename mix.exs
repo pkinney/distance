8944d6a06122861c472ff0d64b6a8afdd47c2e22
Powell Kinney <powell.kinney@toyotaconnected.com>
2017-05-01 12:37:50 -0500

    Bump to version 0.2.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Distance.Mixfile do
 
   def project do
     [app: :distance,
-     version: "0.2.1",
+     version: "0.2.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,

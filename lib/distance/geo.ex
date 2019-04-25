3ff514c1724204908b333a5509ff17cef20555f5
Powell Kinney <pkinney@gmail.com>
2019-04-25 08:47:29 -0500

    Adds distance between a point and a multi-point

--- lib/distance/geo.ex
+++ lib/distance/geo.ex
@@ -19,4 +19,10 @@ defmodule Distance.Geo do
   defp do_distance(%Geo.Point{coordinates: a}, %Geo.Point{coordinates: b}) do
     Distance.distance(a, b)
   end
+
+  defp do_distance(%Geo.MultiPoint{} = a, %Geo.Point{} = b), do: do_distance(b, a)
+  defp do_distance(%Geo.Point{coordinates: a}, %Geo.MultiPoint{coordinates: b}) do
+    Enum.map(b, &(Distance.distance(&1, a)))
+    |> Enum.min
+  end
 end
--- test/geo_distance_test.exs
+++ test/geo_distance_test.exs
@@ -11,11 +11,26 @@ defmodule GeoDistanceTest do
   end
 
   test "distance between a Point and a Point that intersect" do
-    assert_in_delta Distance.distance({3, 1}, {3, 1}), 0, 0.001
+    assert_in_delta Distance.Geo.distance({3, 1}, {3, 1}), 0, 0.001
+  end
+  
+
+  test "distance between a Point and a MultiPoint" do
+    a = "POINT (86 72)" |> Geo.WKT.decode!
+    b = "MULTIPOINT (20 20, 80 80, 20 120)" |> Geo.WKT.decode!
+
+    assert_in_delta Distance.Geo.distance(a, b), 10, 0.001
+    assert_in_delta Distance.Geo.distance(b, a), 10, 0.001
+  end
+  
+  test "distance between a Point and a MultiPoint that intersect" do
+    a = "POINT (20 120)" |> Geo.WKT.decode!
+    b = "MULTIPOINT (20 20, 80 80, 20 120)" |> Geo.WKT.decode!
+
+    assert Distance.Geo.distance(a, b) == 0
+    assert Distance.Geo.distance(b, a) == 0
   end
 
-  # test "distance between a Point and a MultiPoint"
-  # test "distance between a Point and a MultiPoint that intersect"
   # test "distance between a Point and a LineString"
   # test "distance between a Point and a LineString that intersect"
   # test "distance between a Point and a MultiLineString"

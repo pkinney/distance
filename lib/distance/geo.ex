b1dcd1b0c407cb95a04b2d89082ef890d1e8a23c
Powell Kinney <pkinney@gmail.com>
2019-04-29 23:26:54 -0500

    Adds polygon-polygon distance calculation and test

--- lib/distance/geo.ex
+++ lib/distance/geo.ex
@@ -1,12 +1,13 @@
 defmodule Distance.Geo do
+  @type point :: {number, number}
 
   @type geometry ::
-      %Geo.Point{}
-      | %Geo.MultiPoint{}
-      | %Geo.LineString{}
-      | %Geo.MultiLineString{}
-      | %Geo.Polygon{}
-      | %Geo.MultiPolygon{}
+          %Geo.Point{}
+          | %Geo.MultiPoint{}
+          | %Geo.LineString{}
+          | %Geo.MultiLineString{}
+          | %Geo.Polygon{}
+          | %Geo.MultiPolygon{}
 
   @spec distance(geometry, geometry) :: number
   def distance(a, b) do
@@ -21,8 +22,74 @@ defmodule Distance.Geo do
   end
 
   defp do_distance(%Geo.MultiPoint{} = a, %Geo.Point{} = b), do: do_distance(b, a)
+
   defp do_distance(%Geo.Point{coordinates: a}, %Geo.MultiPoint{coordinates: b}) do
-    Enum.map(b, &(Distance.distance(&1, a)))
-    |> Enum.min
+    Enum.map(b, &Distance.distance(&1, a))
+    |> Enum.min()
+  end
+
+  defp do_distance(%Geo.MultiPoint{coordinates: a}, %Geo.MultiPoint{coordinates: b}) do
+    Enum.map(a, fn p1 ->
+      Enum.map(b, fn p2 ->
+        Distance.distance(p1, p2)
+      end)
+      |> Enum.min()
+    end)
+    |> Enum.min()
+  end
+
+  defp do_distance(%Geo.Point{} = a, %Geo.LineString{} = b), do: do_distance(b, a)
+
+  defp do_distance(%Geo.LineString{coordinates: a}, %Geo.Point{coordinates: b}) do
+    Enum.chunk_every(a, 2, 1, :discard)
+    |> Enum.map(fn [p1, p2] ->
+      Distance.segment_distance(b, p1, p2)
+    end)
+    |> Enum.min()
+  end
+
+  defp do_distance(%Geo.MultiPoint{} = a, %Geo.LineString{} = b), do: do_distance(b, a)
+
+  defp do_distance(%Geo.LineString{coordinates: a}, %Geo.MultiPoint{coordinates: b}) do
+    Enum.map(b, fn p ->
+      do_point_linestring_distance(p, a)
+    end)
+    |> Enum.min()
+  end
+
+  defp do_distance(%Geo.Polygon{coordinates: rings_a}, %Geo.Polygon{coordinates: rings_b}) do
+    Enum.map(rings_a, fn a ->
+      Enum.map(rings_b, fn b ->
+        do_linestring_linestring_distance(a, b)
+      end)
+      |> Enum.min()
+    end)
+    |> Enum.min()
+  end
+
+  @spec do_point_linestring_distance(point, list(point)) :: number
+  defp do_point_linestring_distance(a, b) do
+    b_chunks = Enum.chunk_every(b, 2, 1, :discard)
+
+    Enum.map(b_chunks, fn [b1, b2] ->
+      Distance.segment_distance(a, b1, b2)
+    end)
+    |> Enum.min()
+  end
+
+  # Because linear rings duplicate the first point to close, this method can still be used
+  # for Polygon distance calculations
+  @spec do_linestring_linestring_distance(list(point), list(point)) :: number
+  defp do_linestring_linestring_distance(a, b) do
+    a_chunks = Enum.chunk_every(a, 2, 1, :discard)
+    b_chunks = Enum.chunk_every(b, 2, 1, :discard)
+
+    Enum.map(a_chunks, fn [a1, a2] ->
+      Enum.map(b_chunks, fn [b1, b2] ->
+        Distance.segment_segment_distance(a1, a2, b1, b2)
+      end)
+      |> Enum.min()
+    end)
+    |> Enum.min()
   end
 end
--- test/geo_distance_test.exs
+++ test/geo_distance_test.exs
@@ -13,37 +13,91 @@ defmodule GeoDistanceTest do
   test "distance between a Point and a Point that intersect" do
     assert_in_delta Distance.Geo.distance({3, 1}, {3, 1}), 0, 0.001
   end
-  
 
   test "distance between a Point and a MultiPoint" do
-    a = "POINT (86 72)" |> Geo.WKT.decode!
-    b = "MULTIPOINT (20 20, 80 80, 20 120)" |> Geo.WKT.decode!
+    a = "POINT (86 72)" |> Geo.WKT.decode!()
+    b = "MULTIPOINT (20 20, 80 80, 20 120)" |> Geo.WKT.decode!()
 
     assert_in_delta Distance.Geo.distance(a, b), 10, 0.001
     assert_in_delta Distance.Geo.distance(b, a), 10, 0.001
   end
-  
+
   test "distance between a Point and a MultiPoint that intersect" do
-    a = "POINT (20 120)" |> Geo.WKT.decode!
-    b = "MULTIPOINT (20 20, 80 80, 20 120)" |> Geo.WKT.decode!
+    a = "POINT (20 120)" |> Geo.WKT.decode!()
+    b = "MULTIPOINT (20 20, 80 80, 20 120)" |> Geo.WKT.decode!()
 
     assert Distance.Geo.distance(a, b) == 0
     assert Distance.Geo.distance(b, a) == 0
   end
 
-  # test "distance between a Point and a LineString"
-  # test "distance between a Point and a LineString that intersect"
+  test "distance between a Point and a LineString" do
+    a = "LINESTRING(4 8,7 7,8.1 4,8 9,13 10)" |> Geo.WKT.decode!()
+    p1 = %Geo.Point{coordinates: {3.7, 7.6}}
+    p2 = %Geo.Point{coordinates: {4.7, 7.5}}
+    p3 = %Geo.Point{coordinates: {14.5, 8}}
+
+    assert_in_delta Distance.Geo.distance(a, p1), 0.5, 0.001
+    assert_in_delta Distance.Geo.distance(p1, a), 0.5, 0.001
+    assert_in_delta Distance.Geo.distance(a, p2), 0.253243, 0.001
+    assert_in_delta Distance.Geo.distance(p2, a), 0.253243, 0.001
+    assert_in_delta Distance.Geo.distance(a, p3), 2.5, 0.001
+    assert_in_delta Distance.Geo.distance(p3, a), 2.5, 0.001
+  end
+
+  test "distance between a Point and a LineString that intersect" do
+    a = "LINESTRING(4 8,7 7,8.1 4,8 9,12 10)" |> Geo.WKT.decode!()
+    p1 = %Geo.Point{coordinates: {8.1, 4}}
+    p2 = %Geo.Point{coordinates: {10, 9.5}}
+
+    assert Distance.Geo.distance(a, p1) == 0.0
+    assert Distance.Geo.distance(p1, a) == 0.0
+    assert Distance.Geo.distance(a, p2) == 0.0
+    assert Distance.Geo.distance(p2, a) == 0.0
+  end
+
   # test "distance between a Point and a MultiLineString"
   # test "distance between a Point and a MultiLineString that intersect"
   # test "distance between a Point and a Polygon"
+  # test "distance between a Point and a Polygon in its hole"
   # test "distance between a Point and a Polygon that intersect"
   # test "distance between a Point and a MultiPolygon"
   # test "distance between a Point and a MultiPolygon that intersect"
 
-  # test "distance between a MultiPoint and a MultiPoint"
-  # test "distance between a MultiPoint and a MultiPoint that intersect"
-  # test "distance between a MultiPoint and a LineString"
-  # test "distance between a MultiPoint and a LineString that intersect"
+  test "distance between a MultiPoint and a MultiPoint" do
+    a = "MULTIPOINT(3.2 -7.1,6.5 -1,9 -3,12.03 -1.96)" |> Geo.WKT.decode!()
+    b = "MULTIPOINT(2 6,2 -4,12 -2)" |> Geo.WKT.decode!()
+
+    assert_in_delta Distance.Geo.distance(a, b), 0.05, 0.001
+    assert_in_delta Distance.Geo.distance(b, a), 0.05, 0.001
+  end
+
+  test "distance between a MultiPoint and a MultiPoint that intersect" do
+    a = "MULTIPOINT(3.2 -7.1,6.5 -1,9 -3,12.03 -1.96)" |> Geo.WKT.decode!()
+    b = "MULTIPOINT(2 6,2 -4,6.5 -1,12 -2)" |> Geo.WKT.decode!()
+
+    assert Distance.Geo.distance(a, b) == 0.0
+    assert Distance.Geo.distance(b, a) == 0.0
+  end
+
+  test "distance between a MultiPoint and a LineString" do
+    a = "LINESTRING(4 8,7 7,8.1 4,8 9,12 10)" |> Geo.WKT.decode!()
+    b = "MULTIPOINT(3.7 7.6,4.7 7.5,14.5 8,3.2 -7.1,6.5 -1,9 -3,12.03 -1.96)" |> Geo.WKT.decode!()
+
+    assert_in_delta Distance.Geo.distance(a, b), 0.253243, 0.001
+    assert_in_delta Distance.Geo.distance(b, a), 0.253243, 0.001
+  end
+
+  test "distance between a MultiPoint and a LineString that intersect" do
+    a = "LINESTRING(4 8,7 7,8.1 4,8 9,12 10)" |> Geo.WKT.decode!()
+
+    b =
+      "MULTIPOINT(3.7 7.6,4.7 7.5,14.5 8,10 9.5,3.2 -7.1,6.5 -1,9 -3,12.03 -1.96)"
+      |> Geo.WKT.decode!()
+
+    assert Distance.Geo.distance(a, b) == 0.0
+    assert Distance.Geo.distance(b, a) == 0.0
+  end
+
   # test "distance between a MultiPoint and a MultiLineString"
   # test "distance between a MultiPoint and a MultiLineString that intersect"
   # test "distance between a MultiPoint and a Polygon"
@@ -67,11 +121,70 @@ defmodule GeoDistanceTest do
   # test "distance between a MultiLineString and a MultiPolygon"
   # test "distance between a MultiLineString and a MultiPolygon that intersect"
 
-  # test "distance between a Polygon and a Polygon"
-  # test "distance between a Polygon and a Polygon that intersect"
+  test "distance between a Polygon and a Polygon" do
+    a =
+      "POLYGON((-5 3,1 -12,12 -2,8 -5,12 4,9 7,9 3,5 7,-1 6,-5 3),(-3 0,2 5,6 3,7 -2,4 0,1 2,-3 0))"
+      |> Geo.WKT.decode!()
+
+    b = "POLYGON((11 -2, 15 -1, 12 1, 11 -2))" |> Geo.WKT.decode!()
+    c = "POLYGON((11 -2, 15 -1, 12 1, 11 -2),(12 0,12 -1,13 -0.5,12 0))" |> Geo.WKT.decode!()
+
+    assert_in_delta Distance.Geo.distance(a, b), 0.242535, 0.001
+    assert_in_delta Distance.Geo.distance(b, a), 0.242535, 0.001
+
+    assert_in_delta Distance.Geo.distance(a, c), 0.242535, 0.001
+    assert_in_delta Distance.Geo.distance(c, a), 0.242535, 0.001
+  end
+
+  test "distance between a Polygon and an interior Polygon" do
+    a =
+      "POLYGON((-5 3,1 -12,12 -2,8 -5,12 4,9 7,9 3,5 7,-1 6,-5 3),(-3 0,2 5,6 3,7 -2,4 0,1 2,-3 0))"
+      |> Geo.WKT.decode!()
+
+    b =
+      "POLYGON((-4.5 21,-29 -15,17 -17,20 19,-4.5 21),(-8 9,-7 -13,16 -13,14 6,5 17,-8 9))"
+      |> Geo.WKT.decode!()
+
+    assert_in_delta Distance.Geo.distance(a, b), 1.0, 0.001
+    assert_in_delta Distance.Geo.distance(b, a), 1.0, 0.001
+  end
+
+  test "distance between a Polygon and a Polygon that intersect" do
+    a =
+      "POLYGON((-5 3,1 -12,12 -2,8 -5,12 4,9 7,9 3,5 7,-1 6,-5 3),(-3 0,2 5,6 3,7 -2,4 0,1 2,-3 0))"
+      |> Geo.WKT.decode!()
+
+    b = "POLYGON(-10 2, 10 2, 13 -4, -5 -1, -10 2)" |> Geo.WKT.decode!()
+
+    assert Distance.Geo.distance(a, b) == 0.0
+    assert Distance.Geo.distance(b, a) == 0.0
+  end
+
+  test "distance between a Polygon and a Polygon contained in it" do
+    a =
+      "POLYGON((-5 3,1 -12,12 -2,8 -5,12 4,9 7,9 3,5 7,-1 6,-5 3),(-3 0,2 5,6 3,7 -2,4 0,1 2,-3 0))"
+      |> Geo.WKT.decode!()
+
+    b =
+      "POLYGON((-4.5 21,-29 -15,17 -17,20 19,-4.5 21))"
+      |> Geo.WKT.decode!()
+
+    assert Distance.Geo.distance(a, b) == 0.0
+    assert Distance.Geo.distance(b, a) == 0.0
+  end
   # test "distance between a Polygon and a MultiPolygon"
   # test "distance between a Polygon and a MultiPolygon that intersect"
 
   # test "distance between a MultiPolygon and a MultiPolygon"
   # test "distance between a MultiPolygon and a MultiPolygon that intersect"
+
+  # Inside "POLYGON((-5 3,1 -12,12 -2,8 -5,12 4,9 7,9 3,5 7,-1 6,-5 3),(-3 0,2 5,6 3,7 -2,4 0,1 2,-3 0))"
+  # Close Outside LineString, 0.06931089380465308, "LINESTRING(4 8,7 7,8.1 4,8 9,13 10)"
+  # Parallel Outside LineString, 1.41421, "LINESTRING(-4 5,-2.5 6,-4 5.5,-5 5)"
+  # Inside hole LineString "LINESTRING(0 2,1.5 2.5,3 2)"
+  # Interior Point "POINT(3.2 -7.1)"
+  # Point in Hole "POINT(6.5 -1)"
+  # Point outside "POINT(9 -3)"
+  # Point near spike, 0.05, "POINT(12.03 -1.96)"
+  # Outside Polygon "POLYGON((-4.5 21,-29 -15,17 -17,20 19,-4.5 21),(-8 9,-7 -13,16 -13,14 6,5 17,-8 9))"
 end

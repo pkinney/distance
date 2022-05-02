d7beb9c3fcc65771143462b319ad9f2856d6ff28
Powell Kinney <628952+pkinney@users.noreply.github.com>
2022-05-02 13:24:47 -0500

    Bump version to 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Distance.Mixfile do
   def project() do
     [
       app: :distance,
-      version: "1.0.1",
+      version: "1.1.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
